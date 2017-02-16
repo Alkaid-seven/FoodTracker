dcacbaed66f50e9408fb029fd3044cca65f82e7a
Alkaid-seven <fengfeng.awesome@gmail.com>
2017-02-16 22:52:49 +0800

    Implement Edit and Delete behavior.

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import os.log
 
 class MealTableViewController: UITableViewController {
     
@@ -18,11 +19,19 @@ class MealTableViewController: UITableViewController {
     @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
      if let sourceViewController = sender.source as?
         MealViewController, let meal = sourceViewController.meal{
-        // Add a new Meal.
-        let newIndexPath = IndexPath(row: meals.count, section: 0)
-        meals.append(meal)
         
-        tableView.insertRows(at: [newIndexPath], with: .automatic)
+        if let selectedIndexPath = tableView.indexPathForSelectedRow{
+            // Update an existing meal.
+            meals[selectedIndexPath.row] = meal
+            tableView.reloadRows(at: [selectedIndexPath], with: .none)
+        } else {
+            // Add a new Meal.
+            let newIndexPath = IndexPath(row: meals.count, section: 0)
+            meals.append(meal)
+            
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+
+            }
         }
     }
     
@@ -54,7 +63,10 @@ class MealTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Load the Sample data
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        // Load the sample data.
         loadSimpleMeals()
     }
 
@@ -102,17 +114,18 @@ class MealTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            meals.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
@@ -129,14 +142,40 @@ class MealTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        super.prepare(for: segue, sender: sender)
+        
+        switch(segue.identifier ?? "") {
+            case "AddItem":
+                if #available(iOS 10.0, *) {
+                    os_log("Adding a new meal.", log: OSLog.default, type: .debug)
+                } else {
+                    print("Adding a new meal.")
+            }
+        case "ShowDetail":
+            guard let mealDetailViewController = segue.destination as? MealViewController else {
+                fatalError("Unexpected destination: \(segue.destination)")
+            }
+            
+            guard let selectedMealCell = sender as? MealTableViewCell else {
+                fatalError("Unexpected sender: \(sender)")
+            }
+            
+            guard let indexPath = tableView.indexPath(for: selectedMealCell) else {
+                fatalError("The selected cell is not being displayed by the table")
+            }
+            
+            let selectedMeal = meals[indexPath.row]
+            mealDetailViewController.meal = selectedMeal
+        default:
+            fatalError("Unexpected Segue Identifier; \(segue.identifier)")
+        }
     }
-    */
+    
 
 }
